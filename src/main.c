5ced6e1f94da92f866cf465bc955c43408f994b0
João Alberto <hey@joaoalberto.dev>
2024-01-03 22:46:01 -0300

    feat: add basic raylib game loop and rendering

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello");
+#include "../vendor/raylib/src/raylib.h"
+
+int main()
+{
+    SetTargetFPS(60);
+    InitWindow(1000, 1100, "Simple snake game");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        {
+            // Actual code
+        }
+
+        EndDrawing();
+    }
 
     return 0;
 }
